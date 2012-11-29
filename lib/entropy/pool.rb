5d9263fc73c63064c4ecc8a6ec1edb5e77c3a288
Michael Righi <michael@righi.me>
2012-11-29 06:52:35 -0800

    Adds a work-in-progress version of the entropy pool.

---
+++ lib/entropy/pool.rb
@@ -0,0 +1,24 @@
+require 'digest/sha2'
+
+module Entropy
+  class Pool
+    def self.instance
+      @__instance__ ||= new
+    end
+
+    def initialize
+      @hash = Digest::SHA512.hexdigest("#{rand()}#{ObjectSpace.count_objects.to_s}#{GC.stat.to_s}#{Time.now}")
+      @lock = Mutex.new
+    end
+
+    def mix(fresh_blood)
+      result = ''
+      @lock.synchronize do
+        result = @hash = Digest::SHA512.hexdigest("#{@hash}#{Time.now}#{fresh_blood}")
+      end
+      result 
+    end
+
+  end
+
+end
