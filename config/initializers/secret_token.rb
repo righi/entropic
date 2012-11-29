6d55e6c7e8a0b45df94fbc14ab19b7a07e0d2a80
Michael Righi <michael@righi.me>
2012-11-28 23:11:04 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Entropic::Application.config.secret_token = '2fb6dbebd54c7e6eeda294028f83765ea45b5fe474e8056a356cd8d663e57fea52dce44318ffe058c56995cfc1a9d8d1caa5fb34f85570444cf018df46a6d780'
